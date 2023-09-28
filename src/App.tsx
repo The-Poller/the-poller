e5f6d35fe52db10e7ff6963ac663bc687cd7320f
Nick-ncsu <nathomp3@alumni.ncsu.edu>
2023-09-28 16:22:17 -0400

    Initial Commit #poller
    Co-authored-by: Ignacio Rodríguez <ignacio@fosfori.to>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,267 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Endpoints } from '@octokit/types';
+import { useState, useEffect, useMemo } from 'react';
+import { request } from '@octokit/request';
+import Moment from 'react-moment';
+import 'bootstrap/dist/css/bootstrap.css';
+import { ProgressBar, Container, Navbar, Table, Button, Spinner } from 'react-bootstrap';
+import { FaCodePullRequest } from 'react-icons/fa6';
+import { FaCog, FaSmile } from 'react-icons/fa';
+import UserSetting from './UserSetting';
 
-function App() {
+
+type SearchIssuesResponse = Endpoints["GET /search/issues"]["response"]["data"]["items"][0];
+type TableData = { [repoUrl: string]: SearchIssuesResponse[] };
+
+const App: React.FC = () => {
+  const [data, setData] = useState<TableData>({});
+  const [progress, setProgress] = useState<number>(0);
+  const [loadFailed, setLoadFailed] = useState<boolean>(false);
+  const [fullyLoaded, setFullyLoaded] = useState<boolean>(false);
+  const [loadedAt, setLoadedAt] = useState<Date>();
+  const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
+  const [isLoadingData, setIsLoadingData] = useState<boolean>(false);
+  const [noPRsToReview, setNoPRsToReview] = useState(false);
+
+
+  const [users, setUsers] = useState<string[]>([]);
+  const [githubToken, setGithubToken] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+
+  useEffect(() => {
+    const storedUsers = JSON.parse(localStorage.getItem('githubUsers') || '[]');
+    const storedGithubToken = localStorage.getItem('githubToken');
+    const storedUserName = localStorage.getItem('userName');
+
+    if (storedUsers && storedUsers.length) setUsers(storedUsers);
+    if (storedGithubToken) setGithubToken(storedGithubToken);
+    if (storedUserName) setUserName(storedUserName);
+  }, []);
+
+  const resetState = () => {
+    setData({});
+    setProgress(0);
+    setLoadFailed(false);
+    setFullyLoaded(false);
+    setLoadedAt(undefined);
+    setIsLoadingData(false);
+    setNoPRsToReview(false);
+  };
+
+  const isSettingsEmpty = useMemo(() => {
+    return users.length === 0 || !githubToken || !userName;
+  }, [users, githubToken, userName]);
+
+  if (isSettingsEmpty || isSettingsOpen) {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" expand="lg" className="align-items-center">
+          <Container className="d-flex align-items-center justify-content-between">
+            <Navbar.Brand className="d-flex align-items-center">
+              <FaCodePullRequest className="mr-2" /> The Poller™
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="d-flex align-items-center justify-content-end">
+              <UserSetting
+                users={users}
+                setUsers={setUsers}
+                githubToken={githubToken}
+                setGithubToken={setGithubToken}
+                userName={userName}
+                setUserName={setUserName}
+                onModalOpen={() => {
+                  setIsSettingsOpen(true);
+                }}
+                onModalClose={(changesMade) => {
+                  if (changesMade) {
+                    resetState();
+                  };
+                  setIsSettingsOpen(false)
+                }}
+              />
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container className="d-flex justify-content-center align-items-center" style={{ height: 'calc(100vh - 100px)' }}>
+          <div className="text-center">
+            <FaCog size={32} />
+            <p>You must set all settings first</p>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  const loadPRs = () => (async () => {
+    if (isLoadingData) {
+      return;
+    }
+
+    setData({});
+    setIsLoadingData(true);
+    setLoadedAt(undefined);
+    setProgress(0);
+    setLoadFailed(false);
+
+    try {
+      const PRs: TableData = {};
+      for (const [idx, user] of Object.entries(users)) {
+        const result = await request('GET /search/issues', {
+          headers: {
+            authorization: `token ${githubToken}`,
+          },
+          q: `is:open is:pull-request org:jupiterone author:${user} review-requested:${userName} draft:false archived:false`
+        });
+        for (const item of result.data.items || []) {
+          PRs[item.repository_url] ??= [];
+          PRs[item.repository_url].push(item);
+        }
+        setProgress(+idx + 1);
+        setData(PRs);
+      }
+
+      setIsLoadingData(false);
+      setFullyLoaded(true);
+      setLoadedAt(new Date())
+
+      if (Object.keys(PRs).length === 0) {
+        setNoPRsToReview(true);
+      } else {
+        setNoPRsToReview(false);
+      }
+    } catch (error) {
+      setFullyLoaded(false);
+      setLoadFailed(true);
+      setIsLoadingData(false);
+      console.error('Error fetching data:', error);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg" className="align-items-center">
+        <Container className="d-flex align-items-center justify-content-between">
+          <Navbar.Brand className="d-flex align-items-center">
+            <FaCodePullRequest className="mr-2" /> The Poller™
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="d-flex align-items-center justify-content-end">
+            <UserSetting
+              users={users}
+              setUsers={setUsers}
+              githubToken={githubToken}
+              setGithubToken={setGithubToken}
+              userName={userName}
+              setUserName={setUserName}
+              onModalOpen={() => {
+                setIsSettingsOpen(true);
+              }}
+              onModalClose={(changesMade) => {
+                console.log(changesMade);
+                if (changesMade) {
+                  resetState();
+                };
+                setIsSettingsOpen(false)
+              }}
+            />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <div style={{ "display": "flex", justifyContent: 'center', flexDirection: "column", alignItems: 'center', marginTop: '10px' }}>
+          {noPRsToReview ? (
+            <>
+              <FaSmile size={64} color="green" />
+              <p>All PRs have been reviewed already!</p>
+              <Button
+                variant="primary"
+                onClick={loadPRs()}
+                disabled={loadFailed || isLoadingData}
+              >
+                {isLoadingData ? (
+                  <>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    {' '}Loading...
+                  </>
+                ) : loadFailed ? (
+                  'Try again'
+                ) : (
+                  'Refresh data'
+                )}
+              </Button>
+            </>
+          ) : (
+            <Button
+              variant="primary"
+              onClick={loadPRs()}
+              disabled={isLoadingData}
+            >
+              {isLoadingData ? (
+                <>
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                  {' '}Loading...
+                </>
+              ) : loadFailed ? (
+                'Try again'
+              ) : fullyLoaded ? (
+                'Refresh data'
+              ) : (
+                'Load Data'
+              )}
+            </Button>
+          )}
+          <div style={{ width: "50vw", marginTop: '10px', textAlign: 'center' }}>
+            <ProgressBar variant={loadFailed ? 'danger' : (progress !== users.length) ? '' : 'success'} animated={!loadFailed && progress !== users.length} now={loadFailed ? 100 : (progress / users.length) * 100} label={loadFailed ? 'Failed to load all data.' : (progress === users.length) ? "All PRs loaded" : `${users[progress - 1]} ${progress}/${users.length}`} />
+            <span hidden={!loadedAt}>Data loaded <Moment fromNow interval={1000}>{loadedAt}</Moment></span>
+          </div>
+        </div>
+        {Object.entries(data).sort(([repoNameA], [repoNameB]) => repoNameA > repoNameB ? 1 : -1).map(([repositoryUrl, tableData]) => (
+          <div id={repositoryUrl.slice(repositoryUrl.lastIndexOf('/') + 1)} key={repositoryUrl.slice(repositoryUrl.lastIndexOf('/') + 1)}>
+            <h2>
+              <a style={{ textDecoration: 'none', color: 'inherit' }} href={`#${repositoryUrl.slice(repositoryUrl.lastIndexOf('/') + 1)}`}>
+                {repositoryUrl.slice(repositoryUrl.lastIndexOf('/') + 1)}
+              </a>
+            </h2>
+            <Table bordered>
+              <thead>
+                <tr>
+                  <th style={{ width: '80px' }}>PR #</th>
+                  <th style={{ width: '150px' }}>User</th>
+                  <th>Pull Request</th>
+                  <th style={{ width: '150px' }}>Creation time</th>
+                  <th style={{ width: '150px' }}>Updated time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.sort((a, b) => a.number - b.number).map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.number}</td>
+                    <td><a style={{ textDecoration: 'none', display: 'flex', alignItems: 'center', gap: '5px' }} href={item.user?.html_url}> <img height={20} src={item.user?.avatar_url} alt={item.user?.login} />  <span>{item.user?.login}</span></a></td>
+                    <td><a target="_blank" rel="noreferrer" href={item.pull_request?.html_url!}>{item.title}</a></td>
+                    <td><Moment fromNow interval={1000}>{item.created_at}</Moment></td>
+                    <td><Moment fromNow interval={1000}>{item.updated_at}</Moment></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        ))}
+      </Container>
+    </div >
   );
-}
+};
 
 export default App;
---
+++ src/UserSetting.tsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect, } from 'react';
+import { Button, Modal, ListGroup, FormControl, FormLabel, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { FaCog, FaEye, FaEyeSlash, FaPlus } from 'react-icons/fa';
+
+const UserSetting: React.FC<{
+  users: string[],
+  setUsers: React.Dispatch<React.SetStateAction<string[]>>,
+  githubToken: string,
+  setGithubToken: React.Dispatch<React.SetStateAction<string>>,
+  userName: string,
+  setUserName: React.Dispatch<React.SetStateAction<string>>,
+  onModalOpen?: () => void;
+  onModalClose?: (changesMade: boolean) => void;
+}> = ({ users, setUsers, githubToken, setGithubToken, userName, setUserName, onModalOpen, onModalClose }) => {
+  const [newUser, setNewUser] = useState<string>('');
+  const [show, setShow] = useState(false);
+  const [showToken, setShowToken] = useState(false);
+
+  const [changesMade, setChangesMade] = useState(false);
+  const [prevStoredUsers, setPrevStoredUsers] = useState<string[]>([]);
+  const [prevStoredGithubToken, setPrevStoredGithubToken] = useState<string | null>(null);
+  const [prevStoredUserName, setPrevStoredUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!show) {
+      if (onModalClose) onModalClose(changesMade);
+    }
+  }, [changesMade, show])
+
+  const handleAddUser = () => {
+    if (newUser && !users.includes(newUser) && newUser !== userName) {
+      setUsers([...users, newUser]);
+      setNewUser('');
+    }
+  };
+
+  const handleRemoveUser = (user: string) => {
+    setUsers(users.filter(u => u !== user));
+  };
+
+  const handleClose = async () => {
+    localStorage.setItem('githubUsers', JSON.stringify(users));
+    localStorage.setItem('githubToken', githubToken);
+    localStorage.setItem('userName', userName);
+    checkForChanges();
+    setShow(false);
+  }
+
+  const handleShow = () => {
+    if (onModalOpen) onModalOpen();
+    setChangesMade(false);
+
+    const storedUsers = localStorage.getItem('githubUsers');
+    const storedGithubToken = localStorage.getItem('githubToken');
+    const storedUserName = localStorage.getItem('userName');
+
+    if (storedUsers) {
+      setPrevStoredUsers(JSON.parse(storedUsers));
+    }
+    if (storedGithubToken) {
+      setPrevStoredGithubToken(storedGithubToken);
+    }
+    if (storedUserName) {
+      setPrevStoredUserName(storedUserName);
+    }
+    setShow(true);
+  };
+
+  const checkForChanges = () => {
+    if (JSON.stringify(users) !== JSON.stringify(prevStoredUsers) || githubToken !== prevStoredGithubToken || userName !== prevStoredUserName) {
+      setChangesMade(true)
+    }
+  };
+
+  return (
+    <>
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="button-tooltip">Settings</Tooltip>}
+      >
+        <Button
+          variant="outline-light"
+          onClick={handleShow}
+          className="border-0"
+          style={{
+            backgroundColor: 'transparent',
+            color: 'white'
+          }}
+        >
+
+          <FaCog />
+        </Button>
+
+      </OverlayTrigger>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Settings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormLabel>Reviewer Username</FormLabel>
+          <FormControl
+            type="text"
+            placeholder="Enter reviewer username"
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+              localStorage.setItem('userName', e.target.value);
+            }}
+            style={{ marginBottom: '10px' }}
+          />
+          <FormLabel>Github Token</FormLabel>
+          <div style={{ position: 'relative' }}>
+            <FormControl
+              type={showToken ? 'text' : 'password'}
+              placeholder="Enter Github Token"
+              value={githubToken}
+              onChange={(e) => {
+                setGithubToken(e.target.value);
+                localStorage.setItem('githubToken', e.target.value);
+              }}
+              style={{ marginBottom: '10px', paddingRight: '40px' }}
+            />
+            <Button
+              variant="outline-secondary"
+              style={{
+                position: 'absolute',
+                right: '10px',
+                bottom: '7px',
+                padding: '0 5px'
+              }}
+              onClick={() => setShowToken(!showToken)}
+            >
+              {showToken ? <FaEyeSlash /> : <FaEye />}
+            </Button>
+          </div>
+          <FormLabel>Fetch Pull Requests for User</FormLabel>
+          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <FormControl
+              type="text"
+              placeholder="Enter username to fetch pull requests"
+              value={newUser}
+              onChange={(e) => setNewUser(e.target.value)}
+              style={{ flex: 1, marginRight: '10px' }}
+            />
+            <Button onClick={handleAddUser}>
+              <FaPlus />
+            </Button>
+          </div>
+          <FormLabel style={{ display: 'block', marginTop: '10px' }}>Current Users</FormLabel>
+          <ListGroup>
+            {users.map(user => (
+              <ListGroup.Item key={user} style={{
+                display: 'flex', justifyContent: 'space-between', alignItems: 'center'
+              }}>
+                <span>{user}</span>
+                <Button variant="danger" size="sm" onClick={() => handleRemoveUser(user)}>Remove</Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default UserSetting;
